ce79b6d145f9d5f4a3745f8b596324ff5a4099f4
MrT-Branham <sturner@cuhsd.org>
2026-02-11 18:47:35 -0800

    Added files for SplitNamesTest and required files

---
+++ src/SplitNamesTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SplitNamesTest {
+
+    @Test
+    @DisplayName("Split Boy Names")
+    void testSplitBabyNames1 () {
+        try{
+            Path actual = Paths.get("boynames.txt");
+            Path expected = Paths.get("boynamessol.txt");
+            long mismatch = Files.mismatch(actual, expected);
+
+            // print the message based on mismatch result
+            if(mismatch > 1L) {
+                fail("Mismatch occurred in boynames.txt and boynamessol.txt at : " + mismatch);
+            }else {
+                System.out.println("Files matched");
+            }
+
+        }
+        catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Split Girl Names")
+    void testSplitBabyNames2 () {
+        try{
+            Path actual = Paths.get("girlnames.txt");
+            Path expected = Paths.get("girlnamessol.txt");
+            long mismatch = Files.mismatch(actual, expected);
+
+            // print the message based on mismatch result
+            if(mismatch > 1L) {
+                fail("Mismatch occurred in girlnames.txt and girlnamessol.txt at : " + mismatch);
+            }else {
+                System.out.println("Files matched");
+            }
+
+        }
+        catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+}
