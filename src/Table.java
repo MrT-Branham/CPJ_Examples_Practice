c755e9eb3959bb8269e167f3362417e575fb2bdc
MrT-Branham <sturner@cuhsd.org>
2026-04-07 18:09:36 -0700

    Added Furniture, Table, and TableTester.

---
+++ src/Table.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Table extends Furniture{
+    private int legs;
+    //Constructor with no arguments
+
+    //Overloaded constructor with arguments for materials and legs
+    
+
+    public void setLegs(int legs){this.legs = legs; }
+    public int getLegs(){return legs;}
+
+    public String toString(){
+        StringBuilder output = new StringBuilder(String.format(
+                "%s[legs=%d, materials=",
+                getClass().toString(),
+                legs));
+        if (getMaterials().size() == 0){
+            output.append("none]");
+        }
+        else{
+            for(String material: getMaterials()){
+                output.append(material + ",");
+            }
+            output.deleteCharAt(output.length() -1);
+            output.append("]");
+        }
+        return output.toString();
+    }
+
+    public boolean equals(Object otherObject){
+        if (otherObject == null){
+            return false;
+        }
+        if (getClass() != otherObject.getClass()){
+            return false;
+        }
+        Table other = (Table) otherObject;
+        return legs == other.legs &&
+                getMaterials().size() == other.getMaterials().size() &&
+                getMaterials().containsAll(other.getMaterials()); }
+}
---
+++ src/TableTester.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TableTester {
+    public static void main(String[] args){
+        Table kitchen = new Table();
+
+        //Uncomment below to test - commented out to remove compile error
+        //Table coffee = new Table(new ArrayList<String>(Arrays.asList("Mahogany", "Oak")), 4);
+    }
+}
